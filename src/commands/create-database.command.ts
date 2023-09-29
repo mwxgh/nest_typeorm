d577337e0ff22eba0a0bc008c00ceaf1449a2a9a
mwxgh <mwx6996@gmail.com>
2023-09-29 14:11:20 +0700

    sep29: update cli create db

--- src/commands/create-database.command.ts
+++ src/commands/create-database.command.ts
@@ -1,23 +1,26 @@
 import { existsSync } from 'fs';
 import { resolve } from 'path';
 import { createDatabase, DatabaseCreateContext } from 'typeorm-extension';
-import { config as configEnv } from 'dotenv';
-import config from '../config/config';
+import { config } from 'dotenv';
 
-const dbOption = config().database;
-const appOption = config().app;
-const { exit } = process;
-const env = appOption.env || 'development';
-let path = resolve(__dirname, `../../.env.${appOption.env}.local`);
-const database = `${dbOption.db}${env === 'test' ? '_test' : ''}`;
-
+const {
+  env: { NODE_ENV },
+  exit,
+} = process;
+const nodeEnv = NODE_ENV || 'development';
+let path = resolve(__dirname, `../../.env.${nodeEnv}.local`);
 if (!existsSync(path)) path = resolve(__dirname, '../../.env');
 
+let env: any;
 if (existsSync(path)) {
-  const { error } = configEnv({ path });
+  const { parsed, error } = config({ path });
 
   if (error) throw error;
+
+  env = parsed;
 }
+
+const dbName = `${env.DATABASE_DB}${nodeEnv === 'test' ? '_test' : ''}`;
 
 const createDb = async () => {
   const contextDb: DatabaseCreateContext = {
@@ -25,17 +28,17 @@ const createDb = async () => {
     options: {
       charset: 'utf8mb4_unicode_ci',
       type: 'mysql',
-      database,
-      username: dbOption.username,
-      password: dbOption.password,
-      host: dbOption.host,
-      port: dbOption.port,
+      database: dbName,
+      username: env.DATABASE_USER,
+      password: env.DATABASE_PW,
+      host: env.DATABASE_HOST,
+      port: +env.DATABASE_PORT,
       connectTimeout: 5000,
     },
   };
 
   try {
-    console.log(`Create Database ${database} @ ${dbOption.host}`);
+    console.log(`Create Database ${dbName} @ ${env.DATABASE_HOST}`);
     await createDatabase(contextDb);
     console.log('Successfully created!');
     exit(0);
