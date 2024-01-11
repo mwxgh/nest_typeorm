9c2d219c663ddb3268543fe165d5b75aeefecb10
mwxgh <mwx6996@gmail.com>
2024-01-11 16:40:08 +0700

    fix: update connect sql

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
-
 import dbConfig from '@/database/data-source/data-source'
 
 @Module({
@@ -12,22 +11,37 @@ import dbConfig from '@/database/data-source/data-source'
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get('database.host'),
-        port: configService.get('database.port'),
-        username: configService.get('database.username'),
-        password: configService.get('database.password'),
-        database: configService.get('database.db'),
-        entities: [
-          configService.get('appEnv') === 'test'
-            ? 'src/modules/**/entities/*.entity.ts'
-            : 'dist/modules/**/entities/*.entity.js',
-        ],
+      // useFactory: async (configService: ConfigService) => ({
+      //   type: configService.get('DATABASE_DRIVER').toString(),
+      //   host: configService.get('DATABASE_HOST'),
+      //   port: configService.get('DATABASE_PORT'),
+      //   username: configService.get('DATABASE_USER'),
+      //   password: configService.get('DATABASE_PW').toString(),
+      //   database: configService.get('DATABASE_DB').toString(),
+      //   entities: [
+      //     configService.get('APP_ENV') === 'test'
+      //       ? 'src/modules/**/entities/*.entity.ts'
+      //       : 'dist/modules/**/entities/*.entity.js',
+      //   ],
+      //   logging: 'all',
+      //   synchronize: false,
+      //   autoLoadEntities: true,
+      //   charset: 'utf8mb4_unicode_ci',
+      //   supportBigNumbers: true,
+      //   bigNumberStrings: false,
+      // }),
+      useFactory: async () => ({
+        type: dbConfig().type as 'mysql' | 'mariadb' | undefined,
+        host: dbConfig().host,
+        port: Number(dbConfig().port),
+        username: dbConfig().username,
+        password: dbConfig().password,
+        database: dbConfig().database,
+        entities: dbConfig().entities,
         logging: 'all',
         synchronize: false,
         autoLoadEntities: true,
-        charset: 'utf8mb4_unicode_ci',
+        charset: dbConfig().charset,
         supportBigNumbers: true,
         bigNumberStrings: false,
       }),
